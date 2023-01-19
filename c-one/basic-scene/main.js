15334f3d17516dcc40af0073e957d6f9e49bea57
Guillermo Fernandez <bengallme@gmail.com>
2023-01-19 16:05:01 -0600

    using orbit controls to manipulate camera

--- c-one/basic-scene/main.js
+++ c-one/basic-scene/main.js
@@ -1,6 +1,25 @@
 import * as THREE from "three";
 import "./styles.css";
 
+import { OrbitControls } from "three/addons/controls/OrbitControls";
+
+const cursor = {
+    x: 0,
+    y: 0,
+};
+
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = event.clientY / sizes.height - 0.5;
+
+    console.log(cursor);
+});
+
 const scene = new THREE.Scene();
 
 const cube = new THREE.Mesh(
@@ -14,13 +33,8 @@ const axesHelper = new THREE.AxesHelper(3);
 
 scene.add(axesHelper);
 
-const sizes = {
-    width: 800,
-    height: 600,
-};
-
-// const camera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height);
-const camera = new THREE.OrthographicCamera(-1, 1, -1, 1, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height);
+// const camera = new THREE.OrthographicCamera(-1, 1, -1, 1, 0.1, 100);
 
 camera.position.z = 3;
 
@@ -30,20 +44,29 @@ const canvas = document.querySelector(".webgl");
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true;
+
 renderer.setSize(sizes.width, sizes.height);
 
 // Animations
 
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
 function tick() {
-    const elapsedTime = clock.getElapsedTime();
+    // const elapsedTime = clock.getElapsedTime();
 
     renderer.render(scene, camera);
 
-    cube.rotation.y = Math.PI * elapsedTime;
+    // cube.rotation.y = Math.PI * elapsedTime;1
 
-    camera.lookAt(cube.position);
+    // camera.position.x = Math.sin(cursor.x * 2 * Math.PI) * 3;
+    // camera.position.z = Math.cos(cursor.x * 2 * Math.PI) * 3;
+
+    // camera.lookAt(cube.position);
+
+    controls.update();
 
     requestAnimationFrame(tick);
 }
