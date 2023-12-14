a84641f5841fef1cd10aa7b750396b0d48a3018f
Guillermo Fernández <guillermofr@vauxoo.com>
2023-12-14 15:03:39 -0600

    done with basci cameras

--- initial/main.js
+++ initial/main.js
@@ -1,5 +1,16 @@
 import * as THREE from "three";
-import gsap from "gsap";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import gsap from "gsap";
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
 
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
@@ -26,34 +37,50 @@ scene.add(cube1);
 
 // group.position.set(0, 2, -3);
 
-const sizes = {
-  width: 800,
-  height: 600,
-};
-
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1
+// );
 
 camera.position.z = 3;
 
 scene.add(camera);
 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 const axesHelper = new THREE.AxesHelper();
 
 scene.add(axesHelper);
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+  camera.lookAt(cube1.position);
+});
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 
 // const clock = new THREE.Clock();
 
-gsap.to(cube1.position, { duration: 2, delay: 1, x: 2 });
-gsap.to(cube1.position, { duration: 2, delay: 3, x: 0 });
-
+// gsap.to(cube1.position, { duration: 2, delay: 1, x: 2 });
+// gsap.to(cube1.position, { duration: 2, delay: 3, x: 0 });
 
 const tick = () => {
-  //   const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
+
+  // camera.position.x = -1 * cursor.x;
+  // camera.position.y = cursor.y;
+
   //   console.log(elapsedTime);
-  //   cube1.rotation.y = 0.1 * elapsedTime;
+  // cube1.rotation.y = 0.1 * elapsedTime;
+
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
