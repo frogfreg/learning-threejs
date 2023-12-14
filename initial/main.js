e90c8447bff1c069317495f797e1ca69d1128e83
Guillermo Fernández <guillermofr@vauxoo.com>
2023-12-13 19:14:22 -0600

    done with basic animations and gsap

--- initial/main.js
+++ initial/main.js
@@ -1,31 +1,28 @@
 import * as THREE from "three";
+import gsap from "gsap";
 
 const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
 const cube1 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "green" })
-);
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: "red" })
 );
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "blue" })
-);
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "red" })
+// );
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({ color: "blue" })
+// );
 
-cube2.position.set(-1, 0, 0);
-cube3.position.set(1, 0, 0);
+// cube2.position.set(-1, 0, 0);
+// cube3.position.set(1, 0, 0);
 
-const group = new THREE.Group();
+// const group = new THREE.Group();
 
-group.add(cube1);
-group.add(cube2);
-group.add(cube3);
-
-scene.add(group);
+scene.add(cube1);
 
 // group.position.set(0, 2, -3);
 
@@ -47,4 +44,18 @@ scene.add(axesHelper);
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera);
+// const clock = new THREE.Clock();
+
+gsap.to(cube1.position, { duration: 2, delay: 1, x: 2 });
+gsap.to(cube1.position, { duration: 2, delay: 3, x: 0 });
+
+
+const tick = () => {
+  //   const elapsedTime = clock.getElapsedTime();
+  //   console.log(elapsedTime);
+  //   cube1.rotation.y = 0.1 * elapsedTime;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
